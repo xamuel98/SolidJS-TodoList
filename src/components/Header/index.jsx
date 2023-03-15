bee2c845ca70f704c738a041961b6c892cb1f1be
samuel <samuel@Berry.local>
2023-03-15 20:14:25 +0100

    Added github url to Header component and modified Header.module.css

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,7 @@ function Header() {
     return (
         <div class={styles.header}>
             <h1 class={styles.logo}>Todo App</h1>
+            <a class={styles.href} href="https://github.com/xamuel98/SolidJS-TodoList" target='_blank' rel='noreferrer noopener'>Github</a>
         </div>
     )
 }
