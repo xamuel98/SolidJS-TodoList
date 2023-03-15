a9cb87e24363ad2b8c581a5ca60c5f9e0949a188
samuel <samuel@Berry.local>
2023-03-15 20:19:34 +0100

    Fixed mobile responsiveness and changed Header title

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.css';
 function Header() {
     return (
         <div class={styles.header}>
-            <h1 class={styles.logo}>Todo App</h1>
+            <h1 class={styles.logo}>SolidJS Todo App</h1>
             <a class={styles.href} href="https://github.com/xamuel98/SolidJS-TodoList" target='_blank' rel='noreferrer noopener'>Github</a>
         </div>
     )
